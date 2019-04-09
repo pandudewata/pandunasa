199f09ab1b588160270b0d5cccd71f3ba5c13023
fanselyy <43898898+fanselyy@users.noreply.github.com>
2019-04-09 14:59:54 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=EdMb0XQb2ePpKvkK4xC4cC6afzgSDLNIQ4ILIjz7";
         
         $("#loadImage").click(function(){
             $.ajax({
